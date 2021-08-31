b81bc143a0419fceaeff32f3add1d09d3f28c42c
tannguyen <tannguyen.cse19@gmail.com>
2021-08-31 16:26:53 +0700

    skills scroll horizontal

--- script.js
+++ script.js
@@ -11,3 +11,61 @@ $('#popup-container').magnificPopup({
     delegate: 'a', // child items selector, by clicking on it popup will open
     type: 'inline',
 });
+
+// initialize sweet scroll
+document.addEventListener('DOMContentLoaded', () => {
+    // code start here
+    // SweetScroll.create({
+    //     // option declare here
+    //     horizontal: true,
+    //     trigger: 'a[href^="#"]',
+    // }, "#logo-scroll");
+}, false);
+
+const slider = document.querySelector('.logo-container');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+const buttonLeft = document.getElementById('slideLeft');
+const buttonRight = document.getElementById('slideRight');
+
+slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('grab-active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('grab-active');
+});
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('grab-active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX); //scroll-fast
+    slider.scrollLeft = scrollLeft - walk;
+    console.log(walk);
+});
+
+buttonLeft.onmousedown = () => {
+    slider.classList.add('btn-active');
+    slider.scrollLeft -= 200; // can tinh toan lai
+    scrollLeft = slider.scrollLeft;
+};
+
+buttonRight.onmousedown = () => {
+    slider.classList.add('btn-active');
+    slider.scrollLeft += 200;
+    scrollLeft = slider.scrollLeft;
+};
+
+buttonLeft.onmouseup = () => slider.classList.remove('btn-active');
+buttonRight.onmouseup = () => slider.classList.remove('btn-active');
